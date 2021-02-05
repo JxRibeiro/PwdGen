5347ee2a05e1756ebd9c9cbf9d7b9705daafd00a
Joao <joaomarcio.ribeiro@gmail.com>
2021-02-05 19:34:23 -0300

    Update pwdGen.py

--- pwdGen.py
+++ pwdGen.py
@@ -1,13 +1,11 @@
 import string
 import random
 
-
 pwdUpper = string.ascii_uppercase #Letras maiusculas ABC...
 pwdLower = string.ascii_lowercase #Letras minusculas abc...
 pwdDigits = string.digits #Numeros 0123456789
 pwdPunc = string.punctuation #!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-
-
+pwdTest = []
 print('##########################')
 print('#   Gerador de senhas    #')
 print('#------------------------#')
@@ -25,49 +23,19 @@ print('##########################')
 
 pwdEscolha = input('Digite sua escolha (ex 123 ou 124):\n')
 tam = int(input('Qual o tamanho da senha? \n'))
-pwdChars = pwdEscolha
-#pwdChars = string.ascii_letters + string.digits + string.punctuation
+
+pwdChars = ''
 pwd = []
-if pwdEscolha == '1':
-    pwdChars = pwdUpper
-if pwdEscolha == '2':
-    pwdChars = pwdLower
-if pwdEscolha == '3':
-    pwdChars = pwdDigits
-if pwdEscolha == '4':
-    pwdChars = pwdPunc
-
-if pwdEscolha == '12':
-    pwdChars = pwdUpper + pwdLower
-if pwdEscolha == '123':
-    pwdChars = pwdLower + pwdUpper + pwdDigits
-if pwdEscolha == '1234': 
-    pwdChars = pwdDigits + pwdLower + pwdUpper + pwdPunc
-if pwdEscolha == '134': 
-    pwdChars = pwdDigits + pwdLower + pwdPunc
-if pwdEscolha == '13': 
-    pwdChars = pwdDigits + pwdUpper
-if pwdEscolha == '14': 
-    pwdChars = ppwdUpper + pwdPunc
-if pwdEscolha == '234': 
-    pwdChars = pwdDigits + pwdLower  + pwdPunc
-if pwdEscolha == '23': 
-    pwdChars = pwdDigits + pwdLower
-if pwdEscolha == '24': 
-    pwdChars = pwdLower + pwdPunc
-if pwdEscolha == '34': 
-    pwdChars = pwdDigits + pwdPunc
-
-
-
-
-
-
-
-
+escPwd = []
+if '1' in pwdEscolha:
+    pwdChars = pwdChars + pwdUpper
+if '2' in pwdEscolha:
+    pwdChars = pwdChars + pwdLower
+if '3' in pwdEscolha:
+    pwdChars = pwdChars + pwdDigits
+if '4' in pwdEscolha:
+    pwdChars = pwdChars + pwdPunc
 
 for i in range(tam):
-    pwd.append(random.choice(pwdChars))
+      pwd.append(random.choice(pwdChars))
 print(''.join(pwd))
-
-
